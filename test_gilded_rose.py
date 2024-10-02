2ca136eda920778c65f2160951ab7a2d144b4021
Amber Qianyun Wang <88248072+MochitheCoderCat@users.noreply.github.com>
2024-10-01 21:22:43 -0700

    Update test_gilded_rose.py

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,6 +10,21 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+    
+    def test_conjured_items_degrade_normally(self):
+        # Conjured items should degrade faster, but the original code doesn't handle them correctly
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].quality, "Conjured items degrade at the same rate as normal items")
+    
+
+    def test_quality_never_negative_after_sell_by_date(self):
+        items = [Item("+5 Dexterity Vest", -1, -1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        print(gilded_rose.items[0])
+        self.assertGreaterEqual(gilded_rose.items[0].quality, 0)
 
 
 if __name__ == '__main__':
